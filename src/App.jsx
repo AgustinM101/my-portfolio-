aea8f315e64a79279b7c83790e643fef333ca10c
agustinmasso <agustinmasso@hotmail.com>
2025-12-12 18:00:07 -0300

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-950 text-white">
+      <h1 className="text-3xl font-bold text-center py-10">
+        Portfolio en construcción
+      </h1>
+    </div>
+  );
+}
+
+export default App;
